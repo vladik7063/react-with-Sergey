6c21afe2782402450ae9c60db9e9f18e39acc34f
vladik7063 <40918996+vladik7063@users.noreply.github.com>
2022-03-16 19:21:01 +0300

    Create Board.ts

---
+++ 2048/src/utils/createField/Board.ts
@@ -0,0 +1,22 @@
+import { IBoard } from './IBoard';
+
+export class Board implements IBoard {
+  createBoard(size: number): number[][] {
+    throw new Error('Method not implemented.');
+  }
+  getBoard(): number[][] {
+    throw new Error('Method not implemented.');
+  }
+  toTop(): number[][] {
+    throw new Error('Method not implemented.');
+  }
+  toLeft(): number[][] {
+    throw new Error('Method not implemented.');
+  }
+  toBottom(): number[][] {
+    throw new Error('Method not implemented.');
+  }
+  toRight(): number[][] {
+    throw new Error('Method not implemented.');
+  }
+}
